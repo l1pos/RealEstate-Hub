94a6b27aaaf66a7ce644fa552e175d90353e90e9
l1pos <igorchigh@gmail.com>
2025-09-20 21:25:47 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,56 @@
+// data.js
+const properties = [
+    {
+        id: 1,
+        type: "rent",
+        title: "Квартира в центрі Києва",
+        price: "26 000 грн/міс.",
+        description: "Світла 2-кімнатна квартира з новим ремонтом, розташована в самому серці міста, поруч з усіма зручностями.",
+        images: [
+            "images/rent/apt1/1.webp",
+            "images/rent/apt1/2.webp",
+            "images/rent/apt1/3.webp",
+            "images/rent/apt1/4.webp",
+            "images/rent/apt1/5.webp"
+        ]
+    },
+    {
+        id: 2,
+        type: "rent",
+        title: "Сучасна квартира біля метро",
+        price: "21 000 грн/міс.",
+        description: "Зручне розташування, кондиціонер, меблі. Ідеальний варіант для тих, хто цінує комфорт та мобільність.",
+        images: [
+            "images/rent/apt2/1.webp",
+            "images/rent/apt2/2.webp",
+            "images/rent/apt2/3.webp",
+            "images/rent/apt2/4.webp"
+        ]
+    },
+    {
+        id: 3,
+        type: "sale",
+        title: "Затишна 1-к.кв. студія з окремою спальнею",
+        price: "3 263 996 грн",
+        description: "Затишна 1-к.кв. студія з окремою спальнею, Кадетський Гай 10, ЖК «Медовий», новий будинок, євроремонт, консьєрж, закритий двір, 22/25 поверхового будинку, 45/15/17 кв.м.",
+        images: [
+            "images/sale/apt1/1.webp",
+            "images/sale/apt1/2.webp",
+            "images/sale/apt1/3.webp",
+            "images/sale/apt1/4.webp"
+        ]
+    },
+    {
+        id: 4,
+        type: "sale",
+        title: "Будинок 252 м², центр с. Гора",
+        price: "5 164 550 грн",
+        description: "Просторий та затишний будинок в центрі села Гора, вул. Покровська 55А, 252/138/18 м.кв., цегла, 4 кімнати на 1 та 2 поверхах.",
+        images: [
+            "images/sale/apt2/1.webp",
+            "images/sale/apt2/2.webp",
+            "images/sale/apt2/3.webp",
+            "images/sale/apt2/4.webp"
+        ]
+    }
+];
